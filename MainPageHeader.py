ee3ff508d759e4183540d088ddd7369688d2dcc2
Veronika Koval <ve.koval@temabit.com>
2019-01-29 18:06:48 +0200

    tests for header

--- MainPageHeader.py
+++ MainPageHeader.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -8,16 +9,99 @@ from BasePage import BasePage
 class Header(BasePage):
 
     # Locators
-    authorization_btn = (By.CSS_SELECTOR, 'div[data-popup-handler="auth"]')
-    search_field = (By.CSS_SELECTOR, 'input#search-input')
+
+    confirm_location = (By.CSS_SELECTOR, 'a.block-location-action.action-yes.js-hide-location')
+    location = (By.CSS_SELECTOR, 'span[data-span="top"]')
+
+    select_lang = (By.CSS_SELECTOR, 'a[href="#"]')
+
+    feedback_link = (By.CSS_SELECTOR, 'div.block-call-back')
+    phone_num_field = (By.CSS_SELECTOR, 'input[type="tel"]')
+    success_phone_valid = (By.CSS_SELECTOR, 'input.phone-valid]')
+    error_invalid_ph = (By.CSS_SELECTOR, 'span.js-phone-error')
+
+    search_placeholder = (By.CSS_SELECTOR, 'input[placeholder="Поиск..."]')
+    search_field = (By.CSS_SELECTOR, 'input[id="search"]')
+    search_results_block = (By.CSS_SELECTOR, 'div[search_autocomplete]')
+
+
 
     # Actions
 
+    def choose_location(self):
+        """ Choosing current city"""
+        choose_city = self.wait.until(EC.presence_of_element_located(self.confirm_location))
+        choose_city.click()
 
-    def check_search_field(self):
-        """ Checking if the search field is clickable"""
-        search = self.wait.until(EC.element_to_be_clickable(self.search_field))
-        search.send_keys('сухой шампунь')
+    def get_loc_text(self):
+        """Getting location text"""
+        location = self.wait.until(EC.presence_of_element_located(self.location))
+        return location.text
 
+    def change_language(self):
+        """ Changing the language of the site"""
+        change_lang_btn = self.wait.until(EC.presence_of_element_located(self.select_lang))
+        change_lang_btn.click()
+        return self
 
+    def get_site_url(self):
+        """ Getting the site url"""
+        site_url = self.driver.current_url
+        return site_url
+
+    def feedback(self):
+        """ Clicking on feedback link to get feedback pop up"""
+        feedback_btn = self.wait.until(EC.presence_of_element_located(self.feedback_link))
+        feedback_btn.click()
+        return self
+
+    def call_back_invalid(self):
+        """ Checking calling back with the invalid phone number"""
+        phone_field_form = self.wait.until(EC.presence_of_element_located(self.phone_num_field))
+        phone_field_form.click()
+        phone_field_form.send_keys('00000000')
+        phone_field_form.send_keys(Keys.ENTER)
+        return self
+
+    def get_error_msg_text(self):
+        #error = self.wait.until(EC.presence_of_element_located(self.error_invalid_ph))
+        #return error.text
+        return self.wait.until(EC.presence_of_element_located(self.error_invalid_ph)).text
+
+    def call_back_valid(self):
+        """Checking calling back with valid phone number"""
+        phone_field_form = self.wait.until(EC.presence_of_element_located(self.phone_num_field))
+        phone_field_form.click()
+        phone_field_form.send_keys('000000000')
+        phone_field_form.send_keys(Keys.ENTER)
+        return self
+
+    def successfull_call_back(self):
+        # success = self.wait.until(EC.presence_of_element_located(self.success_phone_valid))
+        return self.wait.until(EC.presence_of_element_located(self.success_phone_valid))
+
+    def is_placeholder_present(self):
+        """Checking if the placeholder is present"""
+        try:
+            self.wait.until(EC.visibility_of_element_located(self.search_placeholder))
+            return True
+        except:
+            return False
+
+    def check_search(self):
+        """ Checking the search field"""
+        search = self.wait.until(EC.presence_of_element_located(self.search_field))
+        search.click()
+        search.send_keys('духи')
+        try:
+            self.wait.until(EC.visibility_of_element_located(self.search_results_block))
+            return True
+        except:
+            return False
+
+    def search_with_invalid_query(self):
+        search = self.wait.until(EC.presence_of_element_located(self.search_field))
+        search.click()
+        search.send_keys('xdcydcyu')
+        return self.wait.until(EC.visibility_of_element_located(self.)).text #found and fill in locator
 
--- Test.py
+++ Test.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.common.exceptions import WebDriverException
 
 from BasePage import BasePage
@@ -10,16 +12,52 @@ from MainPageHeader import Header
 
 class Test(BaseTest):
 
-    def test_main_page(self):
-        slaider_locator = (By.CSS_SELECTOR, 'div.header-contact div.button')
-        base_page = BasePage()
-        base_page.click(slaider_locator)
+    # def test_main_page(self):
+    #     slider_locator = (By.CSS_SELECTOR, 'div.header-contact div.button')
+    #     base_page = BasePage()
+    #     base_page.click(slider_locator)
+    #
+    # def test_search_field_is_clickable(self):
+    #     main_page = Header()
+    #     search_locator = main_page.search_field
+    #     # try:
+    #     #     main_page.click(search_locator)
+    #     # except WebDriverException:
+    #     #     print ('Element is not clickable')
 
-    def test_search_field_is_clickable(self):
+    def test_choose_location(self):
         main_page = Header()
-        search_locator = main_page.search_field
-        # try:
-        #     main_page.click(search_locator)
-        # except WebDriverException:
-        #     print ('Element is not clickable')
-        main_page.
+        main_page.choose_location()
+        assert 'Киев' in main_page.get_loc_text()
+
+    def test_changing_lang(self):
+        main_page = Header()
+        main_page.change_language()
+        assert 'ua/' in main_page.get_site_url()
+
+    def test_call_back_unsuccessful(self):
+        main_page = Header()
+        main_page.feedback()\
+            .call_back_invalid()
+        assert 'Введите корректный номер' in main_page.get_error_msg_text()
+
+    def test_call_back_successful(self):
+        main_page = Header()
+        main_page.feedback()\
+            .call_back_valid()\
+            # .successfull_call_back()
+        return main_page.successfull_call_back is True
+
+    def test_placeholder(self):
+        main_page = Header()
+        main_page.is_placeholder_present()
+        return True
+
+    def test_search_results(self):
+        main_page = Header()
+        return main_page.check_search is True
+
+    def test_invalid_search_results(self):
+        pass
+
+    
