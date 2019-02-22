fddbba9db8b449d8088b246fde5d3a4d974c60d6
Veronika Koval <ve.koval@temabit.com>
2019-02-22 18:08:29 +0200

    improvements

--- Pages/MainPageHeader.py
+++ Pages/MainPageHeader.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support import expected_conditions as EC
 
-from BasePage import BasePage
+from Pages.BasePage import BasePage
 
 
 class Header(BasePage):
@@ -29,13 +29,12 @@ class Header(BasePage):
     success_phone_valid = (By.CSS_SELECTOR, 'div.js-submit-result-call-back')
     error_invalid_ph = (By.CSS_SELECTOR, 'span.js-phone-error')
 
-    search_placeholder = (By.CSS_SELECTOR, 'input#search')
     search_field = (By.CSS_SELECTOR, 'input[id="search"]')
     search_results_block = (By.CSS_SELECTOR, 'div.searchautocomplete__autocomplete')
     empty_search_results_block = (By.CSS_SELECTOR, 'div.empty-result')
 
-    our_products_block = (By.CSS_SELECTOR, 'span.eva-icon-arr-down')
-    drop_down_block_our_products = (By.CSS_SELECTOR, 'ul.block-mozayka ')
+    our_projects_block = (By.CSS_SELECTOR, 'span.eva-icon-arr-down')
+    drop_down_block_our_projects = (By.CSS_SELECTOR, 'ul.block-mozayka ')
     mozayka = (By.CSS_SELECTOR, 'a[href="https://mozayka.com.ua/"]')
 
     left_side_menu = (By.CSS_SELECTOR, 'ul.left-menu.js-left-menu')
@@ -80,24 +79,34 @@ class Header(BasePage):
     # Actions
 
     def confirm_default_location(self):
-        """ Choosing current default city"""
-        self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
+        """ Clicking 'Ok' button to confirm the suggested city"""
+        self.wait.until(EC.presence_of_element_located(self.confirm_location_btn),
+            'There is no "Confirm location" button').click()
+        return self
 
     def get_loc_text(self):
-        """Getting location text"""
+        """ Getting the location text from the upper panel, :return: text"""
         return self.wait.until(EC.presence_of_element_located(self.location_top)).text
 
+    def click_choose_another_loc_btn(self):
+        self.wait.until(EC.presence_of_element_located(self.choose_another_location_btn),
+            'There is no "Choose another location" button').click()
+        return self
+
     def choose_location(self):
-        """Choosing your city"""
-        self.wait.until(EC.presence_of_element_located(self.choose_another_location_btn)).click()
-        self.wait.until(EC.presence_of_element_located(self.Lviv)).click()
-        time.sleep(2)
+        """ Clicking 'Choose another location' button,
+        choosing 'Lviv' city as current city from the location drop-down list, :return: main Page"""
+        self.click_choose_another_loc_btn()
+        self.wait.until(EC.presence_of_element_located(self.Lviv), 'There is no such city as Lviv').click()
+        return self
+
+    def get_loc_text_from_popup(self):
+        """ Getting the location text from the location popup, after changing the location, :return: text"""
         return self.wait.until(EC.presence_of_element_located(self.location_text)).text
 
     def choose_random_loc(self):
-        """ Choosing random location from the list of location"""
-        self.wait.until(EC.presence_of_element_located(self.choose_another_location_btn)).click()
-        time.sleep(2)
+        """ Clicking 'Choose another location' button, choosing random location from the list of locations """
+        self.click_choose_another_loc_btn()
 
         all_cities = self.wait.until(EC.presence_of_all_elements_located(self.each_location))
         one_of_the_cities = random.choice(all_cities)
@@ -109,190 +118,149 @@ class Header(BasePage):
             return False
 
     def change_language(self):
-        """ Changing the language of the site to UKR"""
+        """ Changing the language of the site to UKR on the upper panel"""
         self.wait.until(EC.presence_of_element_located(self.select_lang)).click()
+        return self
 
-    def get_site_url(self):
-        """ Getting the site url"""
+    def get_page_url(self):
+        """ Getting the page url, :return: page current URL"""
         return self.driver.current_url
 
-    def call_back_invalid(self):
-        """ Clicking on feedback link to get feedback pop up, checking calling back with the invalid phone number"""
+    def call_back(self, phone_number):
+        """ Clicking on feedback link to get feedback pop up, clicking on the phone field """
         self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
         time.sleep(2)
         self.wait.until(EC.presence_of_element_located(self.feedback_link)).click()
         phone_field_form = self.wait.until(EC.presence_of_element_located(self.phone_num_field))
         phone_field_form.click()
-        phone_field_form.send_keys('00000000')
+        phone_field_form.send_keys(phone_number)
         phone_field_form.send_keys(Keys.ENTER)
+        return self
+
+    def get_error_text_with_invalid_number(self):
+        """ Checking call back functionality with the invalid phone number,
+         :return: text of the error after entering the invalid phone number """
         return self.wait.until(EC.presence_of_element_located(self.error_invalid_ph)).text
 
-    def call_back_valid(self):
-        """ Clicking on feedback link to get feedback pop up, checking calling back with valid phone number"""
-        self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
-        time.sleep(2)
-        self.wait.until(EC.presence_of_element_located(self.feedback_link)).click()
-        phone_field_form = self.wait.until(EC.presence_of_element_located(self.phone_num_field))
-        phone_field_form.click()
-        phone_field_form.send_keys('000000000')
-        phone_field_form.send_keys(Keys.ENTER)
-        time.sleep(2)
+    def get_successful_msg_text_with_valid_number(self):
+        """  Checking call back functionality with the valid phone number,
+         :return: text of the success message after entering the valid phone number """
         return self.wait.until(EC.presence_of_element_located(self.success_phone_valid)).text
 
-    def is_placeholder_present(self):
-        """Checking if the placeholder is present"""
+    def is_search_field_present(self):
+        """Checking if the search field is present"""
         self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
         time.sleep(2)
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.search_placeholder))
-            return True
-        except:
-            return False
+        return self.is_element_present(self.search_field)
 
-    def check_search(self):
-        """ Checking the search field"""
+    def enter_query_into_seqrch_field(self, query):
+        """ Clicking on the search field and entering search query """
         self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
         time.sleep(2)
         search = self.wait.until(EC.presence_of_element_located(self.search_field))
         search.click()
-        search.send_keys('духи')
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.search_results_block))
-            return True
-        except:
-            return False
+        search.send_keys(query)
+        return self
 
-    def search_with_invalid_query(self):
-        """ Checking search results with the invalid query"""
-        self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
-        time.sleep(2)
-        search = self.wait.until(EC.presence_of_element_located(self.search_field))
-        search.click()
-        search.send_keys('xdcydcyu')
+    def is_search_result_block_present(self):
+        """ Clicking on the search field, entering valid search query
+        and checking whether the block with search results is present"""
+        return self.is_element_present(self.search_results_block)
 
-    def get_error_msg_for_query(self):
+    def get_error_msg_for_invalid_query(self):
+        """ Clicking on the search field, entering invalid search query,
+        :return: text of the error for the invalid query """
         return self.wait.until(EC.visibility_of_element_located(self.empty_search_results_block)).text
 
-    def our_products(self):
-        """ Checking the presence of "Our products" block'"""
-        self.wait.until(EC.presence_of_element_located(self.our_products_block)).click()
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.drop_down_block_our_products))
-            return True
-        except:
-            return False
+    def is_our_projects_present(self):
+        """ Clicking on the 'Наши проекты' button, Checking the presence of "Our projects" block'"""
+        self.wait.until(EC.presence_of_element_located(self.our_projects_block)).click()
+        return self.is_element_present(self.drop_down_block_our_projects)
 
-    def main_drop_down_menu(self):
-        """ Checking the presence of the side drop down menu after hovering the main menu item"""
+    def is_main_drop_down_menu_present(self):
+        """ Clicking on 'Все разделы' menu item, checking the presence of the side drop down menu """
         self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
-        time.sleep(2)
         self.wait.until(EC.presence_of_element_located(self.menu_item)).click()
-        try:
-            self.wait.until(EC.presence_of_element_located(self.left_side_menu))
-            return True
-        except:
-            return False
+        return self.is_element_present(self.left_side_menu)
 
-    def hover_menu_items(self):
-        """ Hovering over menu items from left side menu and che checking drop down menu"""
-        self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
-        time.sleep(2)
-        self.wait.until(EC.presence_of_element_located(self.menu_item)).click()
-        time.sleep(2)
-        self.hover_element(self.uhod_mi)
-        self.hover_element(self.parfumeria_mi)
-        self.hover_element(self.tovary_dlja_doma_mi)
-        self.hover_element(self.himija_mi)
-        self.hover_element(self.aksesuaru_mi)
-        self.hover_element(self.kosmetika_dekorat_mi)
-        self.hover_element(self.dlja_muzchin_mi)
-        self.hover_element(self.odezda_mi)
-        self.hover_element(self.bizhuterija_mi)
-        self.hover_element(self.dlja_ditej_mi)
-        try:
-            self.wait.until(EC.presence_of_element_located(self.drop_down_menu))
-            return True
-        except:
-            return False
+    # def hover_menu_items(self):
+    #     """ Hovering over menu items from left side menu and che checking drop down menu"""
+    #     self.wait.until(EC.presence_of_element_located(self.confirm_location_btn)).click()
+    #     time.sleep(2)
+    #     self.wait.until(EC.presence_of_element_located(self.menu_item)).click()
+    #     time.sleep(2)
+    #     self.hover_element(self.uhod_mi)
+    #     self.hover_element(self.parfumeria_mi)
+    #     self.hover_element(self.tovary_dlja_doma_mi)
+    #     self.hover_element(self.himija_mi)
+    #     self.hover_element(self.aksesuaru_mi)
+    #     self.hover_element(self.kosmetika_dekorat_mi)
+    #     self.hover_element(self.dlja_muzchin_mi)
+    #     self.hover_element(self.odezda_mi)
+    #     self.hover_element(self.bizhuterija_mi)
+    #     self.hover_element(self.dlja_ditej_mi)
+    #     try:
+    #         self.wait.until(EC.presence_of_element_located(self.drop_down_menu))
+    #         return True
+    #     except:
+    #         return False
 
-    def product_block_is_present(self):
-        """Checking is product block is present"""
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.product_block))
-            return True
-        except:
-            return False
+    def is_pokupajte_vigodno_present(self):
+        """ Checking is product block present"""
+        return self.is_element_present(self.product_block)
 
-    def check_linejka_tovarov(self):
-        """Checking presence of the block "Linejka tovarov" """
-        try:
-            self.wait.until(EC.presence_of_element_located(self.linejka_tovarov))
-            return True
-        except:
-            return False
+    def is_linejka_tovarov_present(self):
+        """ Checking presence of the block "Linejka tovarov" """
+        return self.is_element_present(self.linejka_tovarov)
 
-    def check_sets(self):
-        """ Checking presence of the "Naboru" block"""
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.naboru_block))
-            return True
-        except:
-            return False
+    def is_nabory_present(self):
+        """ Checking presence of the "Nabory" block"""
+        return self.is_element_present(self.naboru_block)
 
-    def check_brands_block(self):
+    def is_brands_block_present(self):
         """ Checking presence of the "Brands" block """
-        try:
-            self.wait.until(EC.presence_of_element_located(self.brands_block))
-            return True
-        except:
-            return False
+        return self.is_element_present(self.brands_block)
 
-    def subscribe_for_news_unsuccessful(self):
-        """ Checking the ability to subscribe for news with the invalid mail"""
+    def subscribe_for_news(self, email):
+        """ Clicking on the subscribe field, entering email"""
         subscribe = self.wait.until(EC.visibility_of_element_located(self.subscribe_field))
         subscribe.click()
-        subscribe.send_keys('ruzifawomaheximail.com')
+        subscribe.send_keys(email)
         self.wait.until(EC.visibility_of_element_located(self.subscribe_btn)).click()
+        return self
+
+    def get_error_msg_text_newsletters(self):
+        """ Getting error message text after subscribing for news with invalid email,
+         :return: text of the error message"""
         return self.wait.until(EC.visibility_of_element_located(self.subscription_error)).text
 
-    def subscribe_for_news_successful(self):
-        """ Checking the ability to subscribe for news with the valid mail"""
-        subscribe = self.wait.until(EC.visibility_of_element_located(self.subscribe_field))
-        subscribe.click()
-        subscribe.send_keys('baylan.oluwatimilehin@plutocow.com')
-        self.wait.until(EC.visibility_of_element_located(self.subscribe_btn)).click()
+    def get_success_msg_text_newsletters(self):
+        """ Getting error message text after subscribing for news with valid email,
+                 :return: text of the success message"""
         return self.wait.until(EC.visibility_of_element_located(self.subscription_success)).text
 
-    def check_stores_tab(self):
-        """ Opening the "Stores" tab and returning page current URL"""
+    def click_stores_tab(self):
+        """ Clicking the "Stores" tab, :return: page current URL"""
         self.wait.until(EC.presence_of_element_located(self.stores_tab)).click()
         return self.driver.current_url
 
-    def check_news_tab(self):
+    def click_news_tab(self):
         """ Opening the "News" tab and returning page current URL"""
         self.wait.until(EC.presence_of_element_located(self.news_tab)).click()
         return self.driver.current_url
 
-    def check_help_tab(self):
-        """ Opening the "Help" tab and verifying the popup is opened """
+    def click_help_tab(self):
+        """ Opening the "Help" tab on the upper panel and verifying that popup is opened """
         self.wait.until(EC.presence_of_element_located(self.help_tab)).click()
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.help_popup))
-            return True
-        except:
-            return False
+        return self.is_element_present(self.help_popup)
 
-    def check_certificate_tab(self):
+    def click_certificate_tab(self):
         """  Opening the "Certificate" tab and verifying the popup is opened"""
         self.wait.until(EC.presence_of_element_located(self.certificate_tab)).click()
-        try:
-            self.wait.until(EC.visibility_of_element_located(self.certificate_popup))
-            return True
-        except:
-            return False
+        return self.is_element_present(self.certificate_popup)
 
     def checking_wishlist(self):
-        """ Opening the 'Wishlist' and verifying that it isn't available for unregistered account"""
+        """ Opening the 'Wishlist' as unregestered user, :return: page current url to verifying that it isn't available for unregistered account"""
         self.wait.until(EC.presence_of_element_located(self.wishlist_btn)).click()
         return self.driver.current_url
 
